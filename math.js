baa2f1a28ef101fc32f0daa9dd408d1c1e63e809
samayaraj2026 <samayaraj.m@mitrahsoft.in>
2026-03-16 13:20:00 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,23 @@
+
+//add
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(10, 5));
+
+
+//subtract
+function subtract(n, m) {
+    return n - m;
+}
+
+console.log(subtract(40, 20));
+
+
+//Multiply
+function Multiply(n1, n2) {
+    return n1 * n2;
+}
+
+console.log(Multiply(10, 10));
