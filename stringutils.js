7de11bc9219f83b7b50c685a3fe13725c19bb6da
samayaraj2026 <samayaraj.m@mitrahsoft.in>
2026-03-19 15:18:01 +0530

    add capitalize, reverse, trimFunction in stringutils.js

--- stringutils.js
+++ stringutils.js
@@ -1,2 +1,19 @@
-console.log("hello")
-console.log("2");
+// create branch feature/ string-utils
+// creating string.js
+// add capitalize, reverse, trim functions
+// merge late
+
+function capitalize(name){
+    console.log(name.toUpperCase())
+}
+capitalize("javascript")
+
+function reverse(str){
+    return str.split('').reverse().join('')
+}
+console.log(reverse("Hello World!"));
+
+function trimFunc(str){
+    console.log(str.trim())
+}
+trimFunc("hello                 ")
